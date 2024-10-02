075571ca9d58096fbe74269ad1fcc5bf26c9c508
Yzz-0213 <2063223204@qq.com>
2024-10-02 20:31:33 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,7 +2,7 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '老婆早',
     desc: `
       **{{date.DATA}}**
       
@@ -26,11 +26,11 @@ const TEMPLATE_CONFIG = [
       
       今天是我们相识的第{{love_day.DATA}}天
       
-      {{birthday_message.DATA}}
+      
       
       ---
       
-      {{moment_copyrighting.DATA}}
+      
       
       {{poetry_title.DATA}} {{poetry_content.DATA}}
     `
