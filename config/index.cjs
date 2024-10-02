62841186c69b7d30c0d562ec8fc615d0a0585e2b
Yzz-0213 <2063223204@qq.com>
2024-10-02 19:08:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oQkfs6PO2n0gi70YHpbz2MnTN1Sg',
+      id: 'oQkfs6GvT61j7dnCY9GibYWO71M8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'EffiOuD1uxf0f8hT1W0jTX1e6GEjoAN-jnknUyLodAM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
