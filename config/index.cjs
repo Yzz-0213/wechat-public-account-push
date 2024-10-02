d4943555193b9755f98ca8c5f0665219d95b1f7e
Yzz-0213 <2063223204@qq.com>
2024-10-02 17:54:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oQkfs6PO2n0gi70YHpbz2MnTN1Sg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '4huCfRMgZag-VAtUhd1mgna5l4NKFpgBZKp4XVhgcxk',
+      useTemplateId: 'byBos2Xlr3q8poTBV9NzhuqbnZxvx-D4KoM1K_lTdpQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-09',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '4huCfRMgZag-VAtUhd1mgna5l4NKFpgBZKp4XVhgcxk',
+  CALLBACK_TEMPLATE_ID: 'byBos2Xlr3q8poTBV9NzhuqbnZxvx-D4KoM1K_lTdpQ',
 
   CALLBACK_USERS: [
     {
